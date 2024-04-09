4337ff2a3800c691da7a77088e3df38ea404fdb9
Matt Ruddy <matt@calcwise.com>
2024-04-09 17:14:42 +0100

    Add signif helper function

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,2 +1,7 @@
 module ReportHelper
+  # Round a float to a specified number of significant figures
+  def signif(val, digits)
+    return 0 if val.zero?
+    val.round(-(Math.log10(val).ceil - digits))
+  end
 end
