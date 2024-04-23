24201ab444bb5d69775d6603ce8873f3ac36b97e
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-04-23 13:36:02 +0100

    Use real data in home leaderboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
+    @devices = Device.left_joins(:user)
+                     .select('devices.*, users.username, max / (cpus * cores_per_cpu) AS max_per_core')
+                     .order(:max_per_core)
+                     .first(3)
+                     .pluck(:uuid, :platform, :location)
+
   end
 end
 
