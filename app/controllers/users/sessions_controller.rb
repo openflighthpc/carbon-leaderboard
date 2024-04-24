305cf2f0be17e33804119faee6b26b6ab09a59d4
Marco Luo <mailforrgscid@gmail.com>
2024-04-24 10:20:38 +0100

    login fields max length

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,4 +26,13 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def create_by_username
+  end
+
+  def create_by_email
+  end
+  
 end
