26433ef9fa174e26ffc8489097ae2af1f642af52
Marco Luo <mailforrgscid@gmail.com>
2024-04-24 10:11:28 +0100

    username field detection

--- app/javascript/users/sessions/new.js
+++ app/javascript/users/sessions/new.js
@@ -1,2 +1,26 @@
 $(document).ready(() => {
+
+  // username/email input
+  $('#user_username').on('input', function () {
+    let strUser = $(this).val();
+
+    // input email
+    if (strUser.indexOf('@') !== -1) {
+      $('.icon-wrapper.user-field').css({
+        '--icon-image': "url('/assets/ico/email.png')"
+      });
+    
+    // input usernmae
+    } else {
+      //usernmae length limit
+      if (strUser.length > 18) {
+        strUser = strUser.slice(0, 18);
+        $(this).val(strUser);
+      }
+      $('.icon-wrapper.user-field').css({
+        '--icon-image': "url('/assets/ico/user.png')"
+      });
+    }
+  });
+
 });
