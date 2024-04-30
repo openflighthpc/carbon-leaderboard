ea97094a5e88f27b9477e2cb8ee44676d79abd4e
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-04-30 12:00:55 +0100

    Allow updating existing device records

--- app/controllers/data_entry_controller.rb
+++ app/controllers/data_entry_controller.rb
@@ -16,11 +16,20 @@ class DataEntryController < ApplicationController
                  .last
       device = Device.find_by(uuid: json['device_id'])
       if device
-        refresh_page('Device has already been entered')
+        existing = true
       else
+        existing = false
         device = Device.create_from_json(json)
+      end
+      if device.errors.empty?
+        Report.create(device_id: device.uuid,
+                      current: Boavizta.carbon_for_load(device, data['current_load'])
+        )
+        refresh_page("Device #{device.display_name} successfully #{existing ? 'updated' : 'entered'}")
+      else
         refresh_page(device.errors.full_messages.join(', '))
       end
+
     rescue JSON::ParserError
       refresh_page('Invalid payload file format')
     rescue
