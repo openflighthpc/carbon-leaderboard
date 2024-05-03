7a32195ec166b764eeb777bc52a4982867256c11
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-05-03 15:34:09 +0100

    Show list of users nodes on profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,6 @@ class UserController < ApplicationController
   def profile
     @user = User.find_by(username: params[:username])
     @user_owns_profile = user_signed_in? && @user&.id == current_user.id
+    @devices = Device.where(user_id: @user.id)
   end
 end
