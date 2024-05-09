20a6352ff21c0df1447212ff1fd103b5ee49805e
Matt Ruddy <matt@calcwise.com>
2024-05-09 15:30:06 +0100

    Add emission conversion values

---
+++ lib/emision_conversion.rb
@@ -0,0 +1,16 @@
+class EmissionConversion
+  # Multiply carbon value in kg by these to convert to the respective unit
+  FLIGHT = 0.0072       # People flown from London to Hamburg
+  BEEF = 0.01667        # kg of beef produced
+  MANGROVE = 0.0813     # Number of mangroves needed to absorb this amount of carbon in a year
+  GCSE_ENGLISH = 0.1773 # Number of students sitting GCSE english exams
+  DRYING = 0.4167       # Loads of washing dried
+  BIG_MAC = 0.4255      # Big Macs made
+  MCPLANT = 3.4483      # McPlants made
+  DRIVE = 3.4872        # Miles driven in an average car
+  COFFEES = 18.868      # Cups of coffee (with milk, boiling only the water needed)
+  NETFLIX = 55.556      # Episodes streamed on Netflix (30mins each)
+  EMAIL = 250           # Emails sent
+end
+
+
