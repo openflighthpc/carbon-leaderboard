e1eebe435491cea27d4f5aeec2d10803a32e9fce
Matt Ruddy <matt@calcwise.com>
2024-05-09 17:24:36 +0100

    Make comment clearer

--- lib/emision_conversion.rb
+++ lib/emision_conversion.rb
@@ -1,5 +1,6 @@
 class EmissionConversion
-  # Multiply carbon value in kg by these to convert to the respective unit
+  # Values are given in units per kilogram.
+  # To convert to the chosen unit, multiply the corresponding value below by the device carbon output (in kilograms).
   FLIGHT = 0.0072       # People flown from London to Hamburg
   BEEF = 0.01667        # kg of beef produced
   MANGROVE = 0.0813     # Number of mangroves needed to absorb this amount of carbon in a year
