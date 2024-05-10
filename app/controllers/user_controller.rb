24048fd3d2e730033c192a6e0c34f50fbd99ab56
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-05-10 14:19:18 +0100

    Sort devices on user profile by rank

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,8 @@ class UserController < ApplicationController
     @user = User.find_by(username: params[:username])
     @user_owns_profile = user_signed_in? && @user&.id == current_user.id
     @devices = Device.where(user_id: @user.id)
-    @groups = @devices.pluck(:group).uniq
+    @groups = @devices.pluck(:group)
+                      .uniq
+                      .sort_by { |group_id| @devices.find_by(group: group_id).rank }
   end
 end
